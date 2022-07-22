e815182a49cf8187c084d6bd74d716af564acab3
Gonzalo Núñez <92396308+Gonzalonu99@users.noreply.github.com>
2022-07-21 23:08:43 -0300

    initial

--- numen-wines/src/App.js
+++ numen-wines/src/App.js
@@ -1,9 +1,63 @@
+import React, { createContext, useState } from 'react';
+import { GlobalStyles } from './styles/GlobalStyle';
+import NavBar from './components/NavBar';
+import { ThemeProvider } from 'styled-components';
+import Hero from './components/hero';
+import Contador from './components/contador';
+import Wines from './components/wines';
+import Gallery from './components/carousel';
+import './styles/carousel.css';
+import Form from './components/form';
+import { FooterContainer } from './containers/footer';
+import ModalAge from './components/modalAge';
+import useModalAge from './hooks/use-modal-age';
+
+export const ContadorContext = createContext();
 
 function App() {
+  const [cont, setCount] = useState(0);
+  const {modalAgeHandler, modalAgeOpen} = useModalAge();
+
+  const theme = {
+    color: {
+      primary: '#161314',
+      secondary: '#2B2628',
+      tertiary: '#F4F3F3',
+    },
+    font: 'Roboto',
+    colors: {
+      primary: '#FF8126',
+      body: '#FFFFFF',
+      warning: {
+        background: '#C2E8CE',
+        text: '#1F1F1F',
+      },
+      button: '#FFFFFF',
+      latestOffers: {
+        background: '#F7F5F0',
+        oldPrice: '#656565',
+      },
+      ourPartner: '#6090C3',
+    },
+    fonts: {
+      primary: "'Roboto', sans-serif",
+      secondary: "'Cormorant', serif",
+    },
+  };
+
   return (
-    <div className="App">
-      
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <ContadorContext.Provider value={{ cont, setCount }}>
+        <NavBar />
+        <Hero />
+        <Wines />
+        <Gallery />
+        <Form />
+        <FooterContainer />
+        {modalAgeOpen && <ModalAge onConfirm={modalAgeHandler} />}
+      </ContadorContext.Provider>
+    </ThemeProvider>
   );
 }
 
